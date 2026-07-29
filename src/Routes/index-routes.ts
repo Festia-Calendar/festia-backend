7f671998971b5d1c12b3092dc4abce66291236b7
TeerapolGOD <teerapol194@gmail.com>
2026-07-30 01:02:08 +0700

    feat(home) : getBanner and type

--- src/Routes/index-routes.ts
+++ src/Routes/index-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { activityRoutes } from "./activity-route.js"
 import { authRoutes } from "./auth-route.js";
+import { homeRoutes } from "./home-route.js";
 
 const rootRouter: Router = Router();
 
 rootRouter.use(activityRoutes);
 rootRouter.use("/auth",authRoutes);
+rootRouter.use(homeRoutes)
 
 export default rootRouter;
